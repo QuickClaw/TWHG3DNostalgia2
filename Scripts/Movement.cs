a6c02119a3cb7911b59c18cf6b3da4677de62332
Batuhan Ertan <batuhan_ertan99@hotmail.com>
2024-02-09 18:10:38 +0300

    Completed game scripts

--- Scripts/Movement.cs
+++ Scripts/Movement.cs
@@ -3,15 +3,25 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed;
+    private Vector3 movement;
+
+    public Rigidbody rb;
 
     void Update()
     {
-        float leftRight = Input.GetAxisRaw("Horizontal");
-        float forwardBackward = Input.GetAxisRaw("Vertical");
+        //float leftRight = Input.GetAxisRaw("Horizontal");
+        //float forwardBackward = Input.GetAxisRaw("Vertical");
 
-        Vector3 movement = speed * Time.deltaTime * new Vector3(leftRight, 0.0f, forwardBackward);
+        //Vector3 movement = speed * Time.deltaTime * new Vector3(leftRight, 0.0f, forwardBackward);
 
-        transform.Translate(movement, Space.Self);
+        //transform.Translate(movement, Space.Self);
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement.Normalize();
+
+        rb.velocity = movement * speed;
 
         #region Slow 
         if (Input.GetKey(KeyCode.LeftControl))
